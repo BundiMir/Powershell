4cc4736ee9910c882f26688f8014cb2cbb52fd92
A.PBundil <A.PBundil@moravia.com>
2019-03-07 15:03:07 +0100

    new get_dns_record.ps1

--- get_dns_record.ps1
+++ get_dns_record.ps1
@@ -1,4 +1,4 @@
 $ZoneName = "moravia-it.com"
-$Record = "dev-mds"
+$Record = "*testb*"
 Get-DnsServerResourceRecord -ComputerName dcit-cz-1 -ZoneName "moravia-it.com" | Where-Object -FilterScript {$_.Hostname -like $Record} | fl -Property Hostname
 Get-DnsServerResourceRecord -ComputerName dcit-cz-1 -ZoneName "moravia.com" | Where-Object -FilterScript {$_.Hostname -like $Record} | fl -Property Hostname
